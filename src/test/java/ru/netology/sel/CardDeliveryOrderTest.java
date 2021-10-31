e774a62b14fc3569e2a9a7bc6661c72c4e277f42
Vladimir <vladimir.zhevagin@yandex.ru>
2021-10-31 20:29:31 +0300

    add

--- src/test/java/ru/netology/sel/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/sel/CardDeliveryOrderTest.java
@@ -3,36 +3,45 @@ package ru.netology.sel;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 class CardDeliveryOrderTest {
     //git add -f artifacts/app-card-delivery.jar
     //java -jar ./artifacts/app-card-delivery.jar
+
+    static String addData () {
+        LocalDate data = LocalDate.now().plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return data.format(formatter);
+    }
     @Test
-    void shouldRegisterByAccountNumberDOMModification() {
+    void shouldDirectInputOfValues() {
         open("http://localhost:9999");
-        $$(".tab-item").find(exactText("По номеру счёта")).click();
-        $("[name='number']").setValue("4055 0100 0123 4613 8564");
-        $("[name='phone']").setValue("+792000000000");
-        $$("button").find(exactText("Продолжить")).click();
-        $(withText("Успешная авторизация")).shouldBe(visible, Duration.ofMillis(5000));
-        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofMillis(5000));
+        $("[placeholder='Город']").setValue("Нижний Новгород");
+        $("[placeholder='Дата встречи']").setValue(addData());
+        $("[name='name']").setValue("Петров Птр");
+        $("[name='phone']").setValue("+79200000000");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+//        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofMillis(5000));
     }
 
-    @Test
-    void shouldRegisterByAccountNumberVisibilityChange() {
-        open("http://localhost:9999");
-        $$(".tab-item").find(exactText("По номеру счёта")).click();
-        $$("[name='number']").last().setValue("4055 0100 0123 4613 8564");
-        $$("[name='phone']").last().setValue("+792000000000");
-        $$("button").find(exactText("Продолжить")).click();
-        $(withText("Успешная авторизация")).shouldBe(visible, Duration.ofSeconds(5));
-        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofSeconds(5));
-    }
+//    @Test
+//    void shouldRegisterByAccountNumberVisibilityChange() {
+//        open("http://localhost:9999");
+//        $$(".tab-item").find(exactText("По номеру счёта")).click();
+//        $$("[name='number']").last().setValue("4055 0100 0123 4613 8564");
+//        $$("[name='phone']").last().setValue("+792000000000");
+//        $$("button").find(exactText("Продолжить")).click();
+//        $(withText("Успешно! авторизация")).shouldBe(visible, Duration.ofSeconds(5));
+//        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofSeconds(5));
+//    }
 }
 
