27a22178e816be3faf0404ed3abdb0bc6f5dd061
Vladimir <vladimir.zhevagin@yandex.ru>
2021-11-12 18:07:39 +0300

    проверка сообщения и даты

--- src/test/java/ru/netology/sel/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/sel/CardDeliveryOrderTest.java
@@ -6,53 +6,46 @@ import org.openqa.selenium.Keys;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static java.time.Duration.ofSeconds;
 
 class CardDeliveryOrderTest {
-    //git add -f artifacts/app-card-delivery.jar
-    //java -jar ./artifacts/app-card-delivery.jar ./gradlew test --info -Dselenide.headless=true
 
     String deleteString = Keys.chord(Keys.CONTROL, "a") + Keys.DELETE;
 
-    String addDate(String format) {
- //       "dd.MM.yyyy"
-        LocalDate date = LocalDate.now().plusDays(5);
+    String addDate(String format, int dey) {
+        LocalDate date = LocalDate.now().plusDays(dey);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
         return date.format(formatter);
     }
 
-
     @Test
     void shouldDirectInputOfValues() {
+        String meetingDate = addDate("dd.MM.yyyy", 5);
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue("Нижний Новгород");
-        $("[placeholder='Дата встречи']").setValue(deleteString).setValue(addDate("dd.MM.yyyy"));
-        $("[placeholder='Дата встречи']").setValue(addDate("dd.MM.yyyy"));
+        $("[placeholder='Дата встречи']").setValue(deleteString).setValue(meetingDate);
         $("[name='name']").setValue("Петров Петр");
         $("[name='phone']").setValue("+79200000000");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
         $(byText("Успешно!")).shouldBe(visible, ofSeconds(15));
-//        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofMillis(5000));
+        $(".notification__content").should(visible, exactTextCaseSensitive("Встреча успешно забронирована на "
+        + meetingDate));
     }
-
-
 
     @Test
     void shouldIncorrectDateFormat() {
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue("Нижний Новгород");
-        $("[placeholder='Дата встречи']").setValue(deleteString).setValue(addDate("MM.dd.yy"));
+        $("[placeholder='Дата встречи']").setValue(deleteString).setValue(addDate("MM.dd.yy",4));
         $("[name='name']").setValue("Петров Петр");
         $("[name='phone']").setValue("+79200000000");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
         $(byText("Неверно введена дата")).shouldBe(visible, ofSeconds(15));
-        //$(byText("Личный кабинет")).shouldBe(visible, ofSeconds(5));
     }
 }
 
